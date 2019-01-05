d8caf2d4505bd7f4dc308e5a5b2bcde7e442a9b6
ly2011 <1518550424@qq.com>
2019-01-05 12:51:06 +0800

    feat: 改为异步路由加载组件，优化加载文件大小

--- src/router.js
+++ src/router.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import { Router, Route, Switch } from 'dva/router'
+import dynamic from 'dva/dynamic'
 
-import RecommendList from './pages/recommend/recommend'
+/* import RecommendList from './pages/recommend/recommend'
 import RankList from './pages/rank/rank'
 import Search from './pages/search/search'
-import Player from './components/player/player'
+import Player from './components/player/player' */
 
-export default function({ history }) {
+export default function({ history, app }) {
+  const RecommendList = dynamic({
+    app,
+    component: () => import('./pages/recommend/recommend')
+  })
+  const RankList = dynamic({
+    app,
+    component: () => import('./pages/rank/rank')
+  })
+  const Search = dynamic({
+    app,
+    component: () => import('./pages/search/search')
+  })
+  const Player = dynamic({
+    app,
+    component: () => import('./components/player/player')
+  })
   return (
     <Router history={history}>
       <Switch>
