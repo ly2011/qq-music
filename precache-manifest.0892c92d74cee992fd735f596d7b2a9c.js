f2974f638a0507657445930bb8450ec60f23d4c0
ly2011 <1518550424@qq.com>
2019-01-05 12:51:06 +0800

    feat: 改为异步路由加载组件，优化加载文件大小

---
+++ precache-manifest.0892c92d74cee992fd735f596d7b2a9c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a3c60da922ad5a3cfaf",
+    "url": "/static/js/4.2a3c60da.chunk.js"
+  },
+  {
+    "revision": "dc6b2f6896310f0a13a2",
+    "url": "/static/js/0.dc6b2f68.chunk.js"
+  },
+  {
+    "revision": "a9a728d40930ee9b2b89",
+    "url": "/static/js/1.a9a728d4.chunk.js"
+  },
+  {
+    "revision": "7c87b26f2d7f0a8627f1",
+    "url": "/static/js/runtime~main.7c87b26f.js"
+  },
+  {
+    "revision": "7a20ff030385056a6f5f",
+    "url": "/static/js/main.7a20ff03.chunk.js"
+  },
+  {
+    "revision": "cfa3676f87a9f0c5f358",
+    "url": "/static/js/7.cfa3676f.chunk.js"
+  },
+  {
+    "revision": "5e4e2e8974e0b8d9b995",
+    "url": "/static/js/3.5e4e2e89.chunk.js"
+  },
+  {
+    "revision": "03f7142a1f1235c894a9",
+    "url": "/static/js/6.03f7142a.chunk.js"
+  },
+  {
+    "revision": "9fbd16a325a8fe843b4a",
+    "url": "/static/js/5.9fbd16a3.chunk.js"
+  },
+  {
+    "revision": "7a20ff030385056a6f5f",
+    "url": "/static/css/main.e8c19b9c.chunk.css"
+  },
+  {
+    "revision": "cfa3676f87a9f0c5f358",
+    "url": "/static/css/7.0c8cb2df.chunk.css"
+  },
+  {
+    "revision": "9fbd16a325a8fe843b4a",
+    "url": "/static/css/5.b833a9f4.chunk.css"
+  },
+  {
+    "revision": "2a3c60da922ad5a3cfaf",
+    "url": "/static/css/4.dfcd157b.chunk.css"
+  },
+  {
+    "revision": "5e4e2e8974e0b8d9b995",
+    "url": "/static/css/3.6582754b.chunk.css"
+  },
+  {
+    "revision": "a9a728d40930ee9b2b89",
+    "url": "/static/css/1.e144cc5c.chunk.css"
+  },
+  {
+    "revision": "26aa7249ced8d581c95c00ae22ceda88",
+    "url": "/index.html"
+  }
+];
