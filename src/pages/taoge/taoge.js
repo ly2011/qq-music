da52d2ff81dbde713878db1c5bc613d0459807c7
ly2011 <1518550424@qq.com>
2019-01-06 23:12:13 +0800

    feat: 初步完成专辑页

--- src/pages/taoge/taoge.js
+++ src/pages/taoge/taoge.js
@@ -3,15 +3,32 @@ import React, { PureComponent } from 'react'
 import TopBar from './components/top_bar/top_bar'
 import Album from './components/album/album'
 
+import albumData from '../../mock/album.json'
+
+import brandLogo from '../../assets/images/logo.svg'
 import styles from './taoge.module.scss'
 
 class Taoge extends PureComponent {
+  state = {
+    songInfo: {},
+    isOpenSongInfoIntro: true // 是否已展开专辑简介
+  }
   componentDidMount() {
-    console.log(this.props.location.query)
-    console.log(this.props.location.state)
-    console.log(this.props.match.params)
+    // console.log(this.props.location.query)
+    // console.log(this.props.location.state)
+    // console.log(this.props.match.params)
+    const firstObj = Object.values(albumData)[0]
+    this.setState({
+      songInfo: firstObj
+    })
+  }
+  changeSongInfoIntroSwitch = () => {
+    this.setState({
+      isOpenSongInfoIntro: !this.state.isOpenSongInfoIntro
+    })
   }
   render() {
+    const { songInfo, isOpenSongInfoIntro } = this.state
     return (
       <div className={styles['taoge']}>
         <div className={styles['info_box']}>
@@ -44,7 +61,58 @@ class Taoge extends PureComponent {
               </div>
               <a href="javascript:;" className={styles['collect']}>
                 收藏
+                <i className={styles['collect__icon']} />
               </a>
+            </div>
+
+            {songInfo && songInfo.list && (
+              <ul className={styles['song_list']}>
+                {songInfo.list.map((song, index) => (
+                  <li className={styles['song_item']} key={song.songid}>
+                    <div className={styles['song_list__order']}>
+                      <span className={styles['song_list__decimal']}>{index + 1}</span>
+                    </div>
+                    <div className={styles['song_list__bd']}>
+                      <div className={styles['song_list__box']}>
+                        <h3 className={styles['song_list__tit']}>
+                          <span className={styles['song_list__txt']}>{song.songname}</span>
+                        </h3>
+                        <p className={styles['song_list__desc']}>
+                          <span className={styles['song_list__txt']}>
+                            {song.singer && song.singer.map(singer => singer.name).join('/')} . {song.albumdesc}
+                          </span>
+                        </p>
+                      </div>
+                      <div className={styles['song_list__more']}>
+                        <i className={styles['song_list__dot']} />
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+            <div className={styles['check_more']}>点击加载更多歌曲</div>
+            <div className={styles['song_info_tit']}>
+              <div className={styles['song_info_tit__txt']}>专辑简介</div>
+            </div>
+            <div
+              className={`${styles['song_info_intro']} ${
+                styles[isOpenSongInfoIntro ? null : 'song_info_intro--unfold']
+              }`}
+            >
+              <div className={styles['song_info_intro__para']} dangerouslySetInnerHTML={{ __html: songInfo.desc }} />
+              <div
+                className={`${styles['song_info_intro_switch']} ${
+                  styles[isOpenSongInfoIntro ? 'song_info_intro_switch--open' : 'song_info_intro_switch--close']
+                }`}
+                onClick={this.changeSongInfoIntroSwitch}
+              >
+                {isOpenSongInfoIntro ? '展开' : '收起'}
+              </div>
+            </div>
+            <div className={styles.brand}>
+              <img className={styles['brand__logo']} src={brandLogo} alt="" />
+              <p className={styles['brand__name']}>QQ音乐</p>
             </div>
           </div>
         </div>
